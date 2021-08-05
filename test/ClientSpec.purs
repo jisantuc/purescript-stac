1ef44589113fc1f7a2f283d0cad5cbf5d2cc1c37
James Santucci <james.santucci@gmail.com>
2021-08-04 22:03:57 -0600

    separate specs, test one thing

---
+++ test/ClientSpec.purs
@@ -0,0 +1,20 @@
+module Test.ClientSpec where
+
+import Prelude
+import Affjax (printError)
+import Client.Stac (getCollections)
+import Data.Array (length)
+import Data.Either (Either(..))
+import Data.Stac (CollectionsResponse(..))
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (fail, shouldSatisfy)
+
+spec :: Spec Unit
+spec = do
+  describe "collections response is the correct shape" do
+    it "gets the expected /collections shape"
+      $ getCollections "http://localhost:9090"
+      >>= ( case _ of
+            Left err -> fail ("Didn't decode response successfully" <> (printError err))
+            Right (CollectionsResponse { collections }) -> collections `shouldSatisfy` (\x -> length x > 0)
+        )
--- test/Main.purs
+++ test/Main.purs
@@ -1,64 +1,14 @@
 module Test.Main where
 
-import Data.Argonaut
-  ( class DecodeJson
-  , class EncodeJson
-  , decodeJson
-  , encodeJson
-  )
-import Data.Either (Either(..))
-import Data.Stac
-  ( AssetRole
-  , Collection
-  , CollectionItemsResponse
-  , CollectionsResponse
-  , ConformanceClasses
-  , Extent
-  , Interval
-  , Item
-  , Asset
-  , JsonDate
-  , LandingPage
-  , Link
-  , LinkType
-  , MediaType
-  , Provider
-  , ProviderRole
-  , SpatialExtent
-  , TemporalExtent
-  , TwoDimBbox
-  )
 import Effect (Effect)
-import Prelude (class Eq, class Show, Unit, discard, show, (<>), (==))
-import Test.QuickCheck (Result, quickCheck, (<?>))
+import Effect.Aff (launchAff_)
+import Prelude (Unit, bind)
+import Test.Spec.Discovery (discover)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = do
-  quickCheck (\(x :: TwoDimBbox) -> codecRoundTrip "TwoDimBbox" x)
-  quickCheck (\(x :: ProviderRole) -> codecRoundTrip "ProviderRole" x)
-  quickCheck (\(x :: SpatialExtent) -> codecRoundTrip "SpatialExtent" x)
-  quickCheck (\(x :: JsonDate) -> codecRoundTrip "JsonDate" x)
-  quickCheck (\(x :: TemporalExtent) -> codecRoundTrip "TemporalExtent" x)
-  quickCheck (\(x :: Interval) -> codecRoundTrip "Interval" x)
-  quickCheck (\(x :: Extent) -> codecRoundTrip "Extent" x)
-  quickCheck (\(x :: Provider) -> codecRoundTrip "Provider" x)
-  quickCheck (\(x :: LinkType) -> codecRoundTrip "LinkType" x)
-  quickCheck (\(x :: Link) -> codecRoundTrip "Link" x)
-  quickCheck (\(x :: Collection) -> codecRoundTrip "Collection" x)
-  quickCheck (\(x :: CollectionsResponse) -> codecRoundTrip "CollectionResponse" x)
-  quickCheck (\(x :: MediaType) -> codecRoundTrip "MediaType" x)
-  quickCheck (\(x :: AssetRole) -> codecRoundTrip "AssetRole" x)
-  quickCheck (\(x :: Asset) -> codecRoundTrip "Asset" x)
-  quickCheck (\(x :: Item) -> codecRoundTrip "Item" x)
-  quickCheck (\(x :: CollectionItemsResponse) -> codecRoundTrip "CollectionItemsResponse" x)
-  quickCheck (\(x :: LandingPage) -> codecRoundTrip "LandingPage" x)
-  quickCheck (\(x :: ConformanceClasses) -> codecRoundTrip "ConformanceClasses" x)
-
-codecRoundTrip :: forall a. Eq a => Show a => DecodeJson a => EncodeJson a => String -> a -> Result
-codecRoundTrip tag a =
-  let
-    encoded = encodeJson a
-
-    decoded = decodeJson encoded
-  in
-    decoded == Right a <?> "Result for " <> tag <> ": \n" <> show decoded <> "\nnot equal to expected:\n" <> show a
+main =
+  launchAff_ do
+    specs <- discover """Test\..*Spec"""
+    runSpec [ consoleReporter ] specs
